f17a7537c9040845cad0d8f3a56cdf630cbe46da
ionathan sideras <iona8ansideras@gmail.com>
2023-04-30 15:02:49 +0300

    payment

--- src/pay.js
+++ src/pay.js
@@ -10,20 +10,25 @@ export default function Pay({cart}){
                     <input placeholder="First name"/>
                     <input placeholder="Last name"/>
                 </div>
+
                 <input type="email" placeholder="Email"/>
                 <input type="tel" placeholder="Phone number"/>
                 <input placeholder="Cart owner"/>
                 <input type="tel" maxlength="19" placeholder="Cart number"/>
+
                 <div>
                     <input placeholder="MM/YY"/>
                     <input placeholder="CVV/CVC"/>
                 </div>
+
                 <input placeholder="Country"/>
                 <input placeholder="Adress"/>
+
                 <div>
                     <input placeholder="City"/>
                     <input placeholder="ZIP code"/>
                 </div>
+                
                 <button type="submit">submit</button>
             </form>
         </div>
