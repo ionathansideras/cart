116b46a647b72f1236184d4d21fca65ff6c811ca
ionathan sideras <iona8ansideras@gmail.com>
2023-04-30 14:46:13 +0300

    from

--- src/pay.js
+++ src/pay.js
@@ -3,9 +3,29 @@ import React from "react";
 export default function Pay({cart}){
 
     console.log(cart)
-    return(
+    return (
         <div>
-            hello
+            <form>
+                <div>
+                    <input placeholder="First name"/>
+                    <input placeholder="Last name"/>
+                </div>
+                <input type="email" placeholder="Email"/>
+                <input type="tel" placeholder="Phone number"/>
+                <input placeholder="Cart owner"/>
+                <input type="tel" maxlength="19" placeholder="Cart number"/>
+                <div>
+                    <input placeholder="MM/YY"/>
+                    <input placeholder="CVV/CVC"/>
+                </div>
+                <input placeholder="Country"/>
+                <input placeholder="Adress"/>
+                <div>
+                    <input placeholder="City"/>
+                    <input placeholder="ZIP code"/>
+                </div>
+                <button type="submit">submit</button>
+            </form>
         </div>
     )
 }
