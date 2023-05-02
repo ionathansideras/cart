34cabfb3644f2e3c9a8e602bf1ffbcd0bc6cbba6
ionathan sideras <iona8ansideras@gmail.com>
2023-05-02 20:04:50 +0300

    form with state

--- src/pay.js
+++ src/pay.js
@@ -4,63 +4,83 @@ import './paystyle.css'
 
 export default function Pay({cart, total}){
 
-    
+    const [formValues, setFormValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        cartOwner: '',
+        cartNumber: '',
+        mmyy: '',
+        cvvcvc: '',
+        country: '',
+        adress: '',
+        city: '',
+        zipCode: ''
+    })
+
+    function handleForm(e){
+        e.preventDefault();
+    }
+
+
     return (
+
         <div className="all">
-            <form>
+            <form onSubmit={(e) => handleForm(e)}>
                 <div className="divs">
                     <div className="erdiv">
-                        <input placeholder="First name"/>
+                        <input onChange={(e) => setFormValues({...formValues, firstName: e.target.value})} value={formValues.firstName} placeholder="First name"/>
                         <span></span>
                     </div>
                     <div className="erdiv">
-                        <input placeholder="Last name"/>
+                        <input onChange={(e) => setFormValues({...formValues, lastName: e.target.value})} value={formValues.lastName} placeholder="Last name"/>
                         <span></span>
                     </div>
                 </div>
                 <div className="erdiv">
-                    <input type="email" placeholder="Email"/>
+                    <input onChange={(e) => setFormValues({...formValues, email: e.target.value})} value={formValues.email} type="email" placeholder="Email"/>
                     <span></span>
                 </div>
                 <div className="erdiv">
-                    <input type="tel" placeholder="Phone number"/>
+                    <input onChange={(e) => setFormValues({...formValues, phoneNumber: e.target.value})} value={formValues.phoneNumber} type="tel" placeholder="Phone number"/>
                     <span></span>
                 </div>
                 <div className="erdiv">
-                    <input placeholder="Cart owner"/>
+                    <input onChange={(e) => setFormValues({...formValues, cartOwner: e.target.value})} value={formValues.cartOwner} placeholder="Cart owner"/>
                     <span></span>
                 </div>
                 <div className="erdiv">
-                    <input type="tel" maxlength="19" placeholder="Cart number"/>
+                    <input onChange={(e) => setFormValues({...formValues, cartNumber: e.target.value})} value={formValues.cartNumber} type="tel" maxLength="19" placeholder="Cart number"/>
                     <span></span>
                 </div>
 
                 <div className="divs">
                     <div className="erdiv">
-                        <input placeholder="MM/YY"/>                
+                        <input onChange={(e) => setFormValues({...formValues, mmyy: e.target.value})} value={formValues.mmyy} placeholder="MM/YY"/>                
                         <span></span>
                     </div>
                     <div className="erdiv">
-                        <input placeholder="CVV/CVC"/>  
+                        <input onChange={(e) => setFormValues({...formValues, cvvcvc: e.target.value})} value={formValues.cvvcvc} placeholder="CVV/CVC"/>  
                         <span></span>
                     </div>
                 </div>
                 <div className="erdiv">
-                    <input placeholder="Country"/>
+                    <input onChange={(e) => setFormValues({...formValues, country: e.target.value})} value={formValues.country} placeholder="Country"/>
                     <span></span>
                 </div>
                 <div className="erdiv">
-                    <input placeholder="Adress"/>   
+                    <input onChange={(e) => setFormValues({...formValues, adress: e.target.value})} value={formValues.adress} placeholder="Adress"/>   
                     <span></span>
                 </div>
 
                 <div className="divs">
                     <div className="erdiv">
-                        <input placeholder="City"/>
+                        <input onChange={(e) => setFormValues({...formValues, city: e.target.value})} value={formValues.city} placeholder="City"/>
                         <span></span>
                     </div>
                     <div className="erdiv">
-                        <input placeholder="ZIP code"/>
+                        <input onChange={(e) => setFormValues({...formValues, zipCode: e.target.value})} value={formValues.zipCode} placeholder="ZIP code"/>
                         <span></span>
                     </div>
                 </div>
